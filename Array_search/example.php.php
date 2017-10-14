47f24a7ed83d77456498a11549caca71db7c4251
emanuele33 <emanuelegarcia33@gmail.com>
2017-10-13 23:13:45 -0300

    Pesquisa Arrays

---
+++ Array_search/example.php.php
@@ -0,0 +1,6 @@
+<?php 
+
+$arr= array("cor1"=>"Azul","cor2"=>"Vermelho","cor3"=>"Amarelo","cor4"=>"Cinza");
+echo array_search("Vermelho",$arr);
+
+?>
---
+++ in_array/example.php.php
@@ -0,0 +1,10 @@
+<?php  
+
+$arr= array("Azul","Vermelho","Amarelo","Cinza");
+if (in_array("Azul", $arr)) {
+	echo "Encontrada";
+}else{
+	echo "Não Encontrada";
+}
+
+?>
