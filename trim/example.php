3ab4f7389b87ff0a98aa43100603e26dbb9b47fd
wandeson <wandeson36@gmail.com>
2017-10-15 18:12:01 -0300

    teste

--- trim/example.php
+++ trim/example.php
@@ -2,7 +2,7 @@
 $var1 = "     Hello world     ";
 $var2=  "     Hello world     ";
 echo (trim($var1)),PHP_EOL; // o resultado será "Hello world ".
-echo ($var2); // Observe que sem a funcão trim(); Os espaços ficam antes e depois da string "    Hello world     ".
+echo ($var2); // Observe que sem a funcão trim(); Os espaços ficam antes e depois da string "   Hello world    ".
 
 <?
 
