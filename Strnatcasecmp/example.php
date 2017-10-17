575dfaf16c4f0ecc5452845c5e33c8d51ed2a4ca
Eduardo Lonardi <eduardobarbalho04@gmail.com>
2017-10-17 14:05:14 -0300

    Alterações de Pastas

--- Strnatcasecmp/example.php
+++
@@ -1,8 +0,0 @@
-<?php  
-$arr2 = array("pic1","pic2","pic10","pic01","pic100","pic20","pic30","pic200");
-
-echo "Natural order string comparison"."<br>";
-usort($arr2,"strnatcmp");
-
-print_r($arr2);
-?>
