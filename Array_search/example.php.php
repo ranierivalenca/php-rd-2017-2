5c92a8fba2bb6e0631ba2f7b2d21ad61bdb95bf0
emanuele33 <emanuelegarcia33@gmail.com>
2017-10-17 13:52:58 -0300

    Alteração de pastas

--- Array_search/example.php.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$arr= array("cor1"=>"Azul","cor2"=>"Vermelho","cor3"=>"Amarelo","cor4"=>"Cinza");
-echo array_search("Vermelho",$arr);
-
-?>
--- Array_values/example.php.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-
-$arr= array("cor1"=>"Azul","cor2"=>"Vermelho","cor3"=>"Amarelo","cor4"=>"Cinza");
-print_r(array_values($arr));
-
-?>
