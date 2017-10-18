f4d5e1c5d062362f1ec95c451fe77fb2c685df3a
Lucas Emanuel <silverlred12@gmail.com>
2017-10-17 22:26:55 -0300

    alteracao nescessaria

--- asort/exemplo.php/asort.php
+++
@@ -1,7 +0,0 @@
-<?php
-		$frutas = array("a" => "limao", "b" => "laranja", "c" => "banana", "d" => "melancia" => "e" => "abacaxi"=> "f" => "goiaba" => "z" => "pupuasul");
-		asort($frutas);
-		foreach( $frutas as $chave => $valor ){
-		    echo "$chave = $valor\n";
-		}
- 		?>
